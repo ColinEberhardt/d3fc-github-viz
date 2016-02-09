256d43e10fb6cdf08e28d71644cd1c493a3dd0dd
Colin Eberhardt <colin.eberhardt@gmail.com>
2016-02-09 22:48:37 +0000

    updates styling

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
+var forkSvg = '<svg aria-hidden=\'true\' class=\'octicon octicon-repo-forked\' height=\'16\' role=\'img\' version=\'1.1\' viewBox=\'0 0 10 16\' width=\'10\'><path d=\'M8 1c-1.11 0-2 0.89-2 2 0 0.73 0.41 1.38 1 1.72v1.28L5 8 3 6v-1.28c0.59-0.34 1-0.98 1-1.72 0-1.11-0.89-2-2-2S0 1.89 0 3c0 0.73 0.41 1.38 1 1.72v1.78l3 3v1.78c-0.59 0.34-1 0.98-1 1.72 0 1.11 0.89 2 2 2s2-0.89 2-2c0-0.73-0.41-1.38-1-1.72V9.5l3-3V4.72c0.59-0.34 1-0.98 1-1.72 0-1.11-0.89-2-2-2zM2 4.2c-0.66 0-1.2-0.55-1.2-1.2s0.55-1.2 1.2-1.2 1.2 0.55 1.2 1.2-0.55 1.2-1.2 1.2z m3 10c-0.66 0-1.2-0.55-1.2-1.2s0.55-1.2 1.2-1.2 1.2 0.55 1.2 1.2-0.55 1.2-1.2 1.2z m3-10c-0.66 0-1.2-0.55-1.2-1.2s0.55-1.2 1.2-1.2 1.2 0.55 1.2 1.2-0.55 1.2-1.2 1.2z\'></path></svg>'
+var starSvg = '<svg aria-hidden=\'true\' class=\'octicon octicon-star\' height=\'16\' role=\'img\' version=\'1.1\' viewBox=\'0 0 14 16\' width=\'14\'><path d=\'M14 6l-4.9-0.64L7 1 4.9 5.36 0 6l3.6 3.26L2.67 14l4.33-2.33 4.33 2.33L10.4 9.26 14 6z\'></path></svg>'
+
 function updateInfoBox(data) {
     if (data) {
         d3.select('#info-box')
             .style('display', 'block')
             .html('<h4><a href=\'http://github.com/' + data.full_name + '\'>' + data.full_name + '</a></h4>' +
-                '<p><span class=\'stars\'>' + data.stars + '</span><span class=\'forks\'>' + data.forks +
+                '<p>' + starSvg + '<span>' + data.stars + '</span>' + forkSvg + '<span>' + data.forks + '</span></p>' +
                 '<p>' + data.description + '</p>');
     } else {
         d3.select('#info-box')
@@ -37,7 +40,8 @@ d3.csv('data/repos-dump.csv', function(githubData) {
     // the data that will be bound to the chart
     data = {
         githubData: githubData,
-        line: linearFit
+        line: linearFit,
+        diagonal: [[1, 1], [1e6, 1e6]]
     };
 
     // obtain the unique list of languages and create a colour scale
@@ -88,17 +92,25 @@ d3.csv('data/repos-dump.csv', function(githubData) {
         .decorate(function(sel) {
             sel.style({
                 'stroke': '#444',
-                'opacity': function(d) { return highlighted ? (isGrayed(d) ? 0 : 1) : 0; },
+                'opacity': function(d) { return highlighted ? (isGrayed(d) ? 0 : 1) : 0; }
             });
+        });
+
+    var diagonalSeries  = fc.series.line()
+        .xValue(function(d) { return d[0]; })
+        .yValue(function(d) { return d[1]; })
+        .decorate(function(sel) {
+            sel.style('stroke', '#444');
         });
 
     // use  amulti-series to render a line for each linera fit series and a point series
     var multiSeries = fc.series.multi()
-        .series([pointSeries].concat(data.line.map(function(d) { return lineSeries; })))
+        .series([pointSeries, diagonalSeries].concat(data.line.map(function(d) { return lineSeries; })))
         .mapping(function(series, index) {
             switch(series) {
             case pointSeries: return this.githubData;
-            case lineSeries: return this.line[index - 1];
+            case diagonalSeries: return this.diagonal;
+            case lineSeries: return this.line[index - 2];
             }
         });
 
@@ -114,7 +126,7 @@ d3.csv('data/repos-dump.csv', function(githubData) {
         .yNice()
         .yTicks(2, d3.format(',d'))
         .yOrient("left")
-        .margin({left: 50, bottom: 50, right: 40})
+        .margin({left: 60, bottom: 40, right: 40, top: 20})
         .plotArea(multiSeries)
         .decorate(function(selection) {
             // decorate to add the legend
